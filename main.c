b6cd5cf667b7c4553a236f3a3802b3fa04320be5
Guillaume Millot <guillaume.millot@gmail.com>
2020-07-01 15:39:23 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <assert.h>
+
+int my_func(int x) {
+    
+    assert(x > 0);
+    
+    return -x;
+    
+}
+
+int foo(void) {
+    while (1) {
+    }
+}
+
+int test1(void)
+{
+    int x = 0, y = 0;
+    
+    y = my_func(x);
+    
+    return 0;
+}
+
+int test2(void)
+{
+    foo();
+    
+    return 0;
+}
+
+int test3(void)
+{
+    exit(1);
+    
+    return 0;
+}
